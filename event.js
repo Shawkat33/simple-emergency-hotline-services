aba8aee50895ffd907660f7380e42df161a58cfc
Shawkat Islam <shawkat.islam33@gmail.com>
2025-08-28 04:03:42 +0600

    almost there...

--- event.js
+++ event.js
@@ -1,34 +1,30 @@
-const serviceDetail = {
-	title: "",
-	type: "",
-	number: 0,
-	copyStatus: false,
-	favoritedStatus: false,
-};
-
-const copiedNo = [];
-
-const callHistory = [];
-
 const callNowBtn = document.getElementById("callBtn");
 
 document.addEventListener("click", function (e) {
-	if (e.target.tagName.toLowerCase() === "button") {
+	if (
+		e.target.tagName.toLowerCase() === "button" &&
+		e.target.id !== "callRecClr"
+	) {
+		const serviceDetail = {
+			title: "",
+			type: "",
+			number: 0,
+			copyStatus: false,
+			favoritedStatus: false,
+		};
+
 		const clickedBtn = e.target.closest(".cardContent");
 
-		console.log(e.target.id);
+		// console.log(e.target.id);
 
 		const cardTitle = clickedBtn.querySelector(".cardTitle .title").innerText;
 		const cardType = clickedBtn.querySelector(".cardTitle .type").innerText;
-		const cardNumber = parseInt(
-			clickedBtn.querySelector(".serviceNumber h2").innerText
-		);
-
+		const cardNumber = clickedBtn.querySelector(".serviceNumber h2").innerText;
 		serviceDetail.title = cardTitle;
 		serviceDetail.type = cardType;
 		serviceDetail.number = cardNumber;
 
-		console.log(serviceDetail);
+		// console.log(serviceDetail);
 
 		switch (e.target.id) {
 			case "callBtn":
@@ -43,13 +39,13 @@ document.addEventListener("click", function (e) {
 				favoriteBtn();
 				break;
 
-			case "callRecClr":
-				callRecClr();
-				break;
-
 			default:
 				break;
 		}
+	}
+
+	if (e.target.id === "callRecClr") {
+		callRecClr();
 	}
 });
 
@@ -76,11 +72,11 @@ function callBtn(serviceDetail) {
 
 		record.title = serviceDetail.title;
 		record.number = serviceDetail.number;
-		record.time = new Date().toLocaleTimeString;
+		record.time = new Date().toLocaleTimeString();
 
-		callHistory.push(record);
+		// console.log(record);
 
-		console.log(record, callHistory);
+		addRecord(record);
 	}
 }
 
@@ -104,9 +100,34 @@ function copyBtn(serviceDetail) {
 
 		const copyStat = parseInt(document.getElementById("copyStat").innerText);
 
-		const newCopyStat = copyStat++;
+		const newCopyStat = copyStat + 1;
 		document.getElementById("copyStat").innerText = newCopyStat.toString();
 	}
 }
 
 function favoriteBtn() {}
+
+function addRecord(record) {
+	const historyEl = document.createElement("div");
+	historyEl.innerHTML = `<div
+										class="mt-4 p-4 bg-[#FAFAFA] rounded-lg w-full flex justify-between"
+									>
+										<div class="flex flex-col justify-around">
+											<h2 class="font-semibold text-lg text-[#111111]">
+												${record.title}
+											</h2>
+											<p class="text-[#818181]">${record.number}</p>
+										</div>
+										<div class="flex text-center items-center">
+											<h3 class="text-[#111111] text-lg">${record.time}</h3>
+										</div>
+									</div>`;
+
+	const callHistoryParent = document.getElementById("callRecords");
+	callHistoryParent.appendChild(historyEl);
+}
+
+function callRecClr() {
+	const callHistoryParent = document.getElementById("callRecords");
+	callHistoryParent.replaceChildren();
+}
